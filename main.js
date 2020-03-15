498876fb96cde68e01c95d92c2e914a1bec86142
forfxout <forfxout@gmail.com>
2020-03-15 03:08:59 +0300

    update

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const
   parser = require('body-parser'),
   path = require('path'),
   cors = require('cors'),
+  fs = require('fs'),
   _ = require('lodash');
 
 const
@@ -13,7 +14,12 @@ const
 
 app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
-  
+
+app.get('/airports', async (req, res) => {
+  let airports = JSON.parse(fs.readFileSync(path.join(__dirname, 'airports.json')));
+  res.status(200).json(airports);
+})
+
 async function run() {
   let port = process.env.SERVER_PORT;
   let mongoURI = process.env.MONGO_URI;
